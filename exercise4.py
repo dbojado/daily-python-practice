eb470db5989c3d5d4a88363a498243becf0bd144
Daniella Bojado <daniella.bojado@gmail.com>
2020-08-31 12:25:49 -0500

    add exercise4 python file

---
+++ exercise4.py
@@ -0,0 +1,11 @@
+# Generate a random password with length "password_length" 
+# No duplicate characters 
+# No special characters
+# No spaces
+
+import random
+
+char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"
+password_length = 10
+my_new_password =  "".join(random.sample(char, password_length))
+print(my_new_password)
